f806bc273360e6c8bd19996522c1ab34c467163d
BauerMarco <marcba1@web.de>
2024-08-12 15:38:53 +0200

    fix dependency load

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ cmdclass = {'build_sphinx': BuildDoc}
 version = '0.1'
 setup(
     cmdclass=cmdclass,
+    platforms=["Linux"],
+    python_requires=">=3.7",
     # these override conf.py settings
     command_options={
         'build_sphinx': {
             'version': ('setup.py', version),
             'source_dir': ('setup.py', 'docs')}},
+    extras_require={
+        "build_docs": ["sphinx>=3", "sphinx-rtd-theme"]},
 )
 
